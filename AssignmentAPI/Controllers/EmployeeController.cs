b07907361f93608407feeb5ce2e23affbbb03d3b
MysticEnigma18 <162287157+MysticEnigma18@users.noreply.github.com>
2024-07-12 12:50:53 +0400

    Add files via upload

---
+++ AssignmentAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using common.Models;
+using DAL.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AssignmentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private EmployeeDAL _employeeDAL;
+        public EmployeeController()
+        {
+            _employeeDAL = new EmployeeDAL();
+        }
+
+        // GET: api/<EmployeeController>
+        [HttpGet]
+        public IEnumerable<EmployeeModel> Get()
+        {
+            return _employeeDAL.GetAllEmployees();
+        }
+
+        // POST api/<EmployeeController>
+        [HttpPost]
+        public IEnumerable<EmployeeModel> Post([FromBody] EmployeeModel employee)
+        {
+            return _employeeDAL.AddEmployee(employee);
+        }
+
+        // PUT api/<EmployeeController>/5
+        [HttpPut]
+        public IEnumerable<EmployeeModel> Put([FromBody] EmployeeModel employee)
+        {
+            return _employeeDAL.UpdateEmployee(employee);
+        }
+
+        // DELETE api/<EmployeeController>/5
+        [HttpDelete]
+        public IEnumerable<EmployeeModel>  Delete(int id)
+        {
+            return _employeeDAL.DeleteEmployee(id);
+        }
+    }
+}
---
+++ DAL/DAL/EmployeeDAL.cs
@@ -0,0 +1,128 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using DAL.Utils;
+using common.Models;
+
+namespace DAL.DAL
+{
+    public class EmployeeDAL
+    {
+        public IEnumerable<EmployeeModel> GetAllEmployees()
+        {
+            //Open Connection to the DB
+            DbConnection dbConnection = new DbConnection();
+            dbConnection.OpenConnection();
+
+            //Create SQL Command
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT * FROM Employee";
+            cmd.Connection = dbConnection.connection;
+
+            //Create a DataTable to store all of the values of the DB table
+            DataTable dtb = new DataTable();
+
+            //Fill the DataTable
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(dtb);
+
+            //Create list of EmployeeModel objects
+            List<EmployeeModel> list = new List<EmployeeModel>();
+
+            //Iterate through each rows of the DataTable and assign each values of the employee properties to an employee object, then add to the list.
+            foreach (DataRow row in dtb.Rows)
+            {
+                int employeeId = Convert.ToInt32(row["EmployeeId"]);
+                string firstName = row["FirstName"].ToString();
+                string lastName = row["LastName"].ToString();
+                int age = Convert.ToInt32(row["Age"]);
+
+                EmployeeModel employee = new EmployeeModel()
+                {
+                    EmployeeId = employeeId,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Age = age
+                };
+                list.Add(employee);
+            }
+
+        
+            
+            dbConnection.CloseConnection();
+            return list;
+        }
+
+        public IEnumerable<EmployeeModel> AddEmployee(EmployeeModel addEmployee)
+        {
+            //Open Connection to the DB
+            DbConnection dbConnection = new DbConnection();
+            dbConnection.OpenConnection();
+
+            //Create SQL Command
+            string insertQuery = "INSERT INTO Employee (FirstName,LastName,Age) VALUES (@FirstName,@LastName,@Age)";
+
+            using (SqlCommand cmd = new SqlCommand(insertQuery, dbConnection.connection))
+            {
+                // Add parameters and their values
+                cmd.Parameters.AddWithValue("@FirstName", addEmployee.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", addEmployee.LastName);
+                cmd.Parameters.AddWithValue("@Age", addEmployee.Age);
+
+                // Execute the command
+                cmd.ExecuteNonQuery();
+
+            }
+            dbConnection.CloseConnection();
+            return GetAllEmployees();
+        }
+
+        public IEnumerable<EmployeeModel> UpdateEmployee(EmployeeModel updateEmployee)
+        {
+            //Open Connection to the DB
+            DbConnection dbConnection = new DbConnection();
+            dbConnection.OpenConnection();
+
+            string updateQuery = "UPDATE Employee SET FirstName=@FirstName,LastName=@LastName,Age=@Age WHERE EmployeeId=@EmployeeId";
+
+            using (SqlCommand cmd = new SqlCommand(updateQuery, dbConnection.connection))
+            {
+                // Add parameters and their values
+                cmd.Parameters.AddWithValue("@EmployeeId", updateEmployee.EmployeeId);
+                cmd.Parameters.AddWithValue("@FirstName", updateEmployee.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", updateEmployee.LastName);
+                cmd.Parameters.AddWithValue("@Age", updateEmployee.Age);
+
+                // Execute the command
+                cmd.ExecuteNonQuery();
+
+            }
+            dbConnection.CloseConnection();
+            return GetAllEmployees();
+        }
+
+        public IEnumerable<EmployeeModel> DeleteEmployee(int employeeId)
+        {
+            //Open Connection to the DB
+            DbConnection dbConnection = new DbConnection();
+            dbConnection.OpenConnection();
+
+            EmployeeModel DeleteEmployee = new EmployeeModel()
+            {
+                EmployeeId = employeeId
+            };
+
+            string deleteQuery = "DELETE FROM Employee WHERE EmployeeId=@EmployeeId";
+
+            using (SqlCommand cmd = new SqlCommand(deleteQuery, dbConnection.connection))
+            {
+                cmd.Parameters.AddWithValue("@EmployeeId", DeleteEmployee.EmployeeId);
+
+                // Execute the command
+                cmd.ExecuteNonQuery();
+            }
+            dbConnection.CloseConnection();
+            return GetAllEmployees();
+        }
+    }
+}
